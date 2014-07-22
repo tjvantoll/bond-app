94b578c93b496605d8dd35563dfe1680255a164e
TJ VanToll <tj.vantoll@gmail.com>
2014-07-22 17:02:04 -0400

    Reducing the code as much as I can.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,39 +1,15 @@
 (function() {
-	var files = new kendo.data.DataSource({
-		data: [
-			{
-				id: 1,
-				name: "foo.jpg",
-				description: "foo",
-				src: "http://media.giphy.com/media/Pij3hAnY2TNn2/giphy.gif"
-			},
-			{
-				id: 2,
-				name: "bar.png",
-				description: "bar",
-				src: "http://media.giphy.com/media/mWCXHLTASb9W8/giphy.gif"
-			},
-			{
-				id: 3,
-				name: "bang.gif",
-				description: "bang",
-				src: "http://media.giphy.com/media/JtaG8G135ar72/giphy.gif"
-			}
-		]
-	});
-
 	document.addEventListener("deviceready", function () {
 		new kendo.mobile.Application(document.body);
 		$("#files").kendoMobileListView({
-			dataSource: files,
+			dataSource: ["images/dog1.gif", "images/dog2.gif", "images/dog3.gif"],
 			template: $("#file-template").html()
 		});
 		window.app = {
 			showDetails: function(event) {
-				var file = files.get(event.view.params.id),
-					templateHtml = $("#file-details-template").html(),
+				var templateHtml = $("#file-details-template").html(),
 					template = kendo.template(templateHtml);
-				$("#file-details").html(template(file));
+				$("#file-details").html(template(event.view.params.img));
 			}
 		}
 	});
