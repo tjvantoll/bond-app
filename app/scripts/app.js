6482652b84f0bb9ffe65e7e5c1066e1f8aeaa9df
TJ VanToll <tj.vantoll@gmail.com>
2014-07-23 14:33:26 -0400

    Moving to a single view approach

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,15 +2,12 @@
 	document.addEventListener("deviceready", function () {
 		new kendo.mobile.Application(document.body);
 		$("#files").kendoMobileListView({
-			dataSource: ["images/dog1.gif", "images/dog2.gif", "images/dog3.gif"],
+			dataSource: [
+				"images/dog1.gif",
+				"images/dog2.gif",
+				"images/dog3.gif"
+			],
 			template: $("#file-template").html()
 		});
-		window.app = {
-			showDetails: function(event) {
-				var templateHtml = $("#file-details-template").html();
-				var template = kendo.template(templateHtml);
-				$("#file-details").html(template(event.view.params.img));
-			}
-		}
 	});
 }());
